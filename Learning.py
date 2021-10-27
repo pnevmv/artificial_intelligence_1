440800cd87e069ed34c66a88fc42bd3f14af5ace
Данил Смирнов <81740016+pnevmv@users.noreply.github.com>
2021-10-28 00:37:10 +0300

    Update Learning.py

--- Learning.py
+++ Learning.py
@@ -2,12 +2,14 @@ import os
 import numpy as np
 import pandas as pd
 
+#Считывание набора данных
 def read_data(path, filename):
     return pd.read_csv(os.path.join(path, filename))
 
 df = read_data('/data/notebook_files', 'train.csv')
 df.head()
 
+#Сохранение данных под переменными, Х - признаки, у - метки для обучения или проверки модели
 def load_dataset(label_dict):
     train_X = read_data('/data/notebook_files', 'train.csv').values[:,:-2]
     train_y = read_data('/data/notebook_files', 'train.csv')['Activity']
@@ -19,11 +21,15 @@ def load_dataset(label_dict):
 label_dict = {'WALKING':0, 'WALKING_UPSTAIRS':1, 'WALKING_DOWNSTAIRS':2, 'SITTING':3, 'STANDING':4, 'LAYING':5}
 train_X, train_y, test_X, test_y = load_dataset(label_dict)
 
+#Выбор модели
 from sklearn.neighbors import KNeighborsClassifier as mymod
+#Инициализация модели
 model = mymod()
 
+#Обучение модели
 model.fit(train_X, train_y)
 
+#Оценка модели
 yhat = model.predict(test_X)
 
 from sklearn.metrics import classification_report
